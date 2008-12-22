e130acc7b8138c6acc267825f15dbd39138061f5
lidingpku <lidingpku@467fc700-cac3-11dd-89f9-7f0a91924d23>
2008-12-22 04:36:53 +0000

    add semantic drill-down to smwplus settings

--- mediawiki/extensions/SMWBP/LocalSettings.smwbp.smwplus.php
+++ mediawiki/extensions/SMWBP/LocalSettings.smwbp.smwplus.php
@@ -46,6 +46,16 @@ $phpInterpreter = "/usr/local/bin/php";
 #$smwgMessageBroker = "localhost";
 #$smwgWebserviceEndpoint = "localhost:9876"; 
 
+## #######################################
+## SMW Extensions
+## #######################################
+## --- Semantic Drill Down --- 
+# a page for drilling down into the category-based and semantic data of a site, using easily-created filters.
+# http://www.mediawiki.org/wiki/Extension:Semantic_Drilldown
+# svn checkout http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticDrilldown/
+
+require_once( 'extensions/SemanticDrilldown/includes/SD_Settings.php' );
+
 
 
 
