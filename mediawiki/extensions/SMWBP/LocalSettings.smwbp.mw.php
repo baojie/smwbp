e1b6e78121215739e562f06eacaa1897cdc89ce1
lidingpku <lidingpku@467fc700-cac3-11dd-89f9-7f0a91924d23>
2008-12-30 21:28:50 +0000

    installation instructions and configuration file for MW

---
+++ mediawiki/extensions/SMWBP/LocalSettings.smwbp.mw.php
@@ -0,0 +1,15 @@
+<?php
+
+#################################################################
+# This file is generated the basic configuration for MediaWiki
+
+############################
+## MediaWiki (MW)
+#   svn checkout http://svn.wikimedia.org/svnroot/mediawiki/branches/REL1_13/phase3
+## #######################################
+
+# disallow anonymous edit
+$wgGroupPermissions['*']['edit'] = false;  # restrict anonymous edit
+
+
+?>
