7fa81597ff75ea0e3c69e2b84ef8a2640195b818
lidingpku <lidingpku@467fc700-cac3-11dd-89f9-7f0a91924d23>
2009-01-21 05:54:18 +0000

    add new extensions * TetherlessMap (partial) * AskMany * Semantic Drill Down (moved from smwplus)

---
+++ mediawiki/extensions/SMWBP/LocalSettings.smwbp.rpi.php
@@ -0,0 +1,54 @@
+<?php
+
+#################################################################
+# This file is generated for typical configuration on MW ans SWM extensions
+
+
+## #######################################
+## MW Extensions
+## #######################################
+
+## --- Widgets --- 
+# Allows adding free-type widgets to the wiki by editing pages in Widget namespace
+# http://www.mediawiki.org/wiki/Extension:Widgets
+# svn checkout http://mediawiki-widgets.googlecode.com/svn/trunk/ Widgets
+
+require_once("$IP/extensions/Widgets/Widgets.php");
+
+
+
+## #######################################
+## SMW Extensions
+## #######################################
+
+## --- Semantic Drill Down --- 
+# a page for drilling down into the category-based and semantic data of a site, using easily-created filters.
+# http://www.mediawiki.org/wiki/Extension:Semantic_Drilldown
+# svn checkout http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/SemanticDrilldown/
+
+require_once( 'extensions/SemanticDrilldown/includes/SD_Settings.php' );
+
+## --- TetherlessMap--- 
+# adding map with SMW
+#  http://www.mediawiki.org/wiki/Extension:Tetherless_Map
+#  svn checkout http://smwbp.googlecode.com/svn/trunk/mediawiki/extensions/TetherlessMap/
+
+# you need to configure the following two lines of code in LocalSettings.php
+#$wgGoogleMapsKey = "......"; # enter your Google Maps API key here
+#$wgLocalPath= "......";  #enter your host server address
+
+require_once ("$IP/extensions/TetherlessMap/Individual_Location.php");
+require_once ("$IP/extensions/TetherlessMap/GoogleMapClick.php");
+require_once ("$IP/extensions/TetherlessMap/GoogleMapMultiObjects.php");
+
+
+
+## --- AskMany --- 
+# distributed query on SMW
+# http://tw.rpi.edu/wiki/Help:AskMany
+#   svn checkout http://smwbp.googlecode.com/svn/trunk/mediawiki/extensions/AskMany/
+
+require_once('extensions/AskManyExtension/includes/SMW_AM_Main.php');
+enableAskMany();
+
+?>
