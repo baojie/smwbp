7b3ba52c594d204b17bf7f127c2fa010f5b8f73e
lidingpku <lidingpku@467fc700-cac3-11dd-89f9-7f0a91924d23>
2009-01-21 19:52:05 +0000

    add upload to as default mw settings.

--- mediawiki/extensions/SMWBP/LocalSettings.smwbp.mw.php
+++ mediawiki/extensions/SMWBP/LocalSettings.smwbp.mw.php
@@ -8,6 +8,8 @@
 #   svn checkout http://svn.wikimedia.org/svnroot/mediawiki/branches/REL1_13/phase3
 ## #######################################
 
+############################
+#  permissions
 # disallow anonymous edit
 $wgGroupPermissions['*']['edit'] = false;  # restrict anonymous edit
 
@@ -15,6 +17,31 @@ $wgGroupPermissions['*']['edit'] = false;  # restrict anonymous edit
 $wgGroupPermissions['user']['delete'] = true;  # allow user delete
 $wgGroupPermissions['user']['move'] = true;  # allow user move
 $wgGroupPermissions['user']['refresh'] = true;  # allow user refresh
+
+
+############################
+# upload
+$wgEnableUploads       = true;  //enable upload
+
+/** Warn if uploaded files are larger than this (in bytes), or false to disable*/
+$wgUploadSizeWarning = false;
+
+/** This is a flag to determine whether or not to check file extensions on upload. */
+$wgCheckFileExtensions = false;
+
+/**
+ * If this is turned off, users may override the warning for files not covered
+ * by $wgFileExtensions.
+ */
+$wgStrictFileExtensions = false;
+
+/** File extensions. */
+$wgFileExtensions = array( 'png', 'gif', 'jpg', 'jpeg' , 'ppt', 'pdf', 'ps', 'ep
+s', 'doc','xls', 'owl', 'zip', 'gz', 'txt', 'tex', 'bib', 'cls', 'sty' );
+
+/** verify mime type */
+$wgVerifyMimeType= false;
+
 
 
 ?>
